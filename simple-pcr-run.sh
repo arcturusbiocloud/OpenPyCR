f932d5ac8b6ee2ea0651edbd750c64426bcaa15d
luisbebop <luisbebop@gmail.com>
2015-03-09 19:04:49 -0700

    Bash script to run PCR process and monitor the output.

---
+++ simple-pcr-run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd /root/OpenPyCR
+
+python3 openpycr.py send -p Programs/CoolBlock.pcr
+PCR_STATUS=""
+while [ "$PCR_STATUS" != "Program complete." ]; do
+  PCR_STATUS="$(python3 openpycr.py status)"
+  echo $PCR_STATUS
+  sleep 1
+done
