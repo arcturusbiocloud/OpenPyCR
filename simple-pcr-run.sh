440152f34aad085e2e1a42da28d18ee887b40057
luisbebop <luisbebop@gmail.com>
2015-04-03 18:18:15 -0700

    added script with final holding temperature. that way we can keep the PCR cold or hot after a script execution.

--- simple-pcr-run.sh
+++ simple-pcr-run.sh
@@ -2,7 +2,7 @@
 
 cd /root/OpenPyCR
 
-python3 openpycr.py send -p Programs/CoolBlock.pcr
+python3 openpycr.py send -p Programs/ArcFreezer.pcr
 PCR_STATUS=""
 while [ "$PCR_STATUS" != "Program complete." ]; do
   PCR_STATUS="$(python3 openpycr.py status)"
